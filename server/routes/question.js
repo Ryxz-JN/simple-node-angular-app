e428585f24a761356b1c8f87872531afa93c5e7d
Florian Höhne <florian@mac-fho.local>
2019-06-26 10:23:43 +0200

    add debugging config

--- server/routes/question.js
+++ server/routes/question.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post("/question", (req, res) => {
+  res.send('create resource here');
+});
+
+router.put("/question", (req, res) => {
+  res.send('update resource here');
+});
+
+router.delete("/question", (req, res) => {
+  res.send('remove resource here');
+});
+
 module.exports = router;
